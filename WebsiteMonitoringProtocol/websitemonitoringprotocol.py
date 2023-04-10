ce4147035e9e2c2374f3c1621564ba0160565572
Zak Reynolds <zakreynolds96@gmail.com>
2023-04-10 13:16:12 +0100

    This commit shows the sample protocol generated which constantly queries Glastonbury festival site for changes and automatically sends out an email notification upon any change. This has now been transferred onto a headless RPi via ssh.

--- WebsiteMonitoringProtocol/websitemonitoringprotocol.py
+++ WebsiteMonitoringProtocol/websitemonitoringprotocol.py
@@ -5,15 +5,15 @@ from bs4 import BeautifulSoup
 from email.message import EmailMessage
 import ssl
 import smtplib
-
-
-
-
+import time
 
 URL_TO_MONITOR = "https://glastonbury.seetickets.com/content/extras" #change this to the URL you want to monitor
 
+# write me a function to send emails through python
 
-DELAY_TIME = 2 # seconds
+
+
+DELAY_TIME = 1 # seconds
 
 # the following is a processing function to alter the format of the GET request into something more readable for humans and to remove annoying tags which change with every page query
 
@@ -39,19 +39,52 @@ def process_html(string):
 
 # set up of email details and email content
 
-email_sender = 'zakburner22@gmail.com'
-email_password = 'beqstfnzlskidqtr' # fake password inputted here for sharing with world on GitHub
-email_receiver = 'zakreynolds96@gmail.com'
+
+
+
+ # instantiating email objects for each recipient using the imported email module and then tagging the relevant attributes. The SSL class adds extra ecurity onto the email contents
+
+email_sender = 'zakreynolds96@gmail.com'
+email_password = 'fakepassword' # fake password inputted here for sharing with world on GitHub
+email_receiver1 = 'zak@cellcraft.com'
+email_receiver2 = 'ariannasmithbdc@gmail.com'
+email_receiver3 = 'kane_reynolds@hotmail.com'
+email_receiver4 = 'kairobinson00@icloud.com'
 
 subject = 'Website has changed!'
-body = """Get your ass over to the glasto site right now https://glastonbury.seetickets.com/content/extras"""
+body = """Get your ass over to the glasto site right now https://glastonbury.seetickets.com/content/extras
+Arianwen Smith 1123314535 EN49FE
+Zak Reynolds 3288228557 EN76RS
+Kane Reynolds 1711364100 EN76RS
+Kai Robinson 1469727581 EN106FE"""
 
  # instantiating email object using the imported email module and then tagging the relevant attributes. The SSL class adds extra ecurity onto the email contents
-em = EmailMessage()
-em['From'] = email_sender
-em['To'] = email_receiver
-em['Subject'] = subject
-em.set_content(body)
+em1 = EmailMessage()
+em1['From'] = email_sender
+em1['To'] = email_receiver1
+em1['Subject'] = subject
+em1.set_content(body)
+context = ssl.create_default_context()
+
+em2 = EmailMessage()
+em2['From'] = email_sender
+em2['To'] = email_receiver2
+em2['Subject'] = subject
+em2.set_content(body)
+context = ssl.create_default_context()
+
+em3 = EmailMessage()
+em3['From'] = email_sender
+em3['To'] = email_receiver3
+em3['Subject'] = subject
+em3.set_content(body)
+context = ssl.create_default_context()
+
+em4 = EmailMessage()
+em4['From'] = email_sender
+em4['To'] = email_receiver4
+em4['Subject'] = subject
+em4.set_content(body)
 context = ssl.create_default_context()
 
 def webpage_was_changed(): 
@@ -64,6 +97,7 @@ def webpage_was_changed():
     # create the previous_content.txt if it doesn't exist
     if not os.path.exists("previous_content.txt"): #does this file exist from a previous loop, if not then create it
         open("previous_content.txt", 'w+').close()
+        
 
     # open the previous context file and assign it to file handle which is passed to previous_response_html and then closed
     filehandle = open("previous_content.txt", 'r')
@@ -90,20 +124,25 @@ def webpage_was_changed():
     # while loop to run continuously with a delay sleep time of 5 seconds per cycle. __name__ == "__main__" means the main function will only be called when the script is the main script running and not just a module imported into another script
 def main():  
     while True:
-        try:
-            if webpage_was_changed():
-                print('Yay')
-                
-                with smtplib.SMTP_SSL('smtp.gmail.com',465,context=context) as smtp:
-                    smtp.login(email_sender,email_password)
-                    smtp.sendmail(email_sender,email_receiver,em.as_string())
+            try:
+        
+                if webpage_was_changed():
+                    print('Yay')
+                    with smtplib.SMTP_SSL('smtp.gmail.com',465,context=context) as smtp:
+                        smtp.login(email_sender,email_password)
+                        smtp.sendmail(email_sender,email_receiver1,em1.as_string())
+                        smtp.sendmail(email_sender,email_receiver2,em2.as_string())
+                        smtp.sendmail(email_sender,email_receiver3,em3.as_string())
+                        smtp.sendmail(email_sender,email_receiver4,em4.as_string())
+                      
+                   
                             
                     print("Successfully sent email")           
-            else:
-                print('Nothing yet')
-        except:
-            print('Error')
+                else:
+                    print('Nothing yet')
+            except:
+                print('Error in sending email')
         
-        time.sleep(DELAY_TIME)
+            time.sleep(DELAY_TIME)
 if __name__ == "__main__":
     main()
